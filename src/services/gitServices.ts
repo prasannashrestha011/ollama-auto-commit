9cabb7aff6ae816e3459ce5bb3d1090e7cd6fdc4
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-03-25 17:59:48 +0545

    refactor: replace git execSync service with vscode git api service and moved interface to types/git.ts

--- src/services/gitServices.ts
+++ src/services/gitServices.ts
@@ -1,55 +1,79 @@
-import * as vscode from "vscode";
-import { execSync } from "child_process";
+import { GitApi, GitExtension, Repository, StagedInfo } from "../types/git";
+import { window, workspace, extensions } from "vscode";
 
-export interface StagedInfo {
-    diff: string;
-    files: string[]
-    additions: number;
-    deletions: number;
-}
 export class GitService {
-    private workSpaceRoot: string;
-    constructor(workSpaceRoot: string) {
-        this.workSpaceRoot = workSpaceRoot;
+    private repo: Repository;
+    private gitExtension: GitExtension;
+    constructor(repo: Repository, gitExtension: GitExtension) {
+        this.repo = repo;
+        this.gitExtension = gitExtension;
     }
-    static fromWorkSpace(): GitService | null {
-        const folders = vscode.workspace.workspaceFolders;
-        if (!folders || folders.length === 0) {
+
+    static fromActiveEditor(): GitService | null {
+        const ext = extensions.getExtension<GitExtension>("vscode.git");
+        if (!ext?.isActive) {
+            window.showErrorMessage("Git extension is not active, please install or activate it");
             return null;
         }
-        return new GitService(folders[0].uri.fsPath);
-    }
-    getStagedInfo(maxLength: number = 4000): StagedInfo {
-        const diff = this.run("git diff --cached");
-        const stat = this.run("git diff --cached --stat");
 
-        const files = this.run("git diff --cached --name-only").split("\n").map(f => f.trim()).filter(Boolean);
+        const git = ext.exports.getApi(1);
+        if (!git.repositories.length) {
+            window.showErrorMessage("No git repositories found");
+            return null;
+        }
+        const repo = GitService.resolveRepo(git);
+
+        if (!repo) {
+            window.showErrorMessage("Repository not found with current file buffer");
+            return null;
+        }
+        return new GitService(repo, ext.exports);
+
+
+    }
+    private static resolveRepo(git: GitApi): Repository | null {
+        const activeEditor = window.activeTextEditor;
+
+        /*
+         * returns repository of active Editor
+        */
+        if (activeEditor) {
+            const fileUri = activeEditor.document.uri;
+            const repoForFile = git.repositories.find(repo => fileUri.fsPath.startsWith(repo.rootUri.fsPath));
+            if (repoForFile) { return repoForFile; }
+        }
+
+        const workSpaceEditor = activeEditor ?
+            workspace.getWorkspaceFolder(activeEditor.document.uri) :
+            workspace.workspaceFolders?.[0];
+        if (workSpaceEditor) {
+            const workSpaceUri = workSpaceEditor.uri;
+            const repoForWorkSpace = git.repositories.find(repo => workSpaceUri.fsPath.startsWith(repo.rootUri.fsPath));
+            if (repoForWorkSpace) { return repoForWorkSpace; }
+        }
+        if (git.repositories.length !== 0) {
+            return git.repositories[0];
+        }
+        return null;
+
+    }
+    async getStagedInfo(maxLength: number = 4000): Promise<StagedInfo> {
+        const rawDiff = await this.repo.diff(true);
+        const { additions, deletions } = this.parseDiff(rawDiff);
+        const files = this.repo.state.indexChanges.map(change => workspace.asRelativePath(change.uri));
+        const truncateDiff = rawDiff.length > maxLength ? rawDiff.slice(0, maxLength) + "\n\n[diff truncated...]" : rawDiff;
+        return { additions, deletions, diff: rawDiff, files };
+    }
+
+    parseDiff(diff: string): { additions: number, deletions: number } {
         let additions = 0;
         let deletions = 0;
-        const statMatch = stat.match(/(\d+) insertion|(\d+) deletion/g) || [];
-        statMatch.forEach((m) => {
-            const n = parseInt(m);
-            if (m.includes("insertion")) { additions += n; }
-            if (m.includes("deletion")) { deletions += n; }
-        });
 
-        // Truncate diff if needed
-        const truncatedDiff =
-            diff.length > maxLength
-                ? diff.slice(0, maxLength) + "\n\n[diff truncated...]"
-                : diff;
-
-        return { diff: truncatedDiff, files, additions, deletions };
-
-
+        for (const line of diff.split("\n")) {
+            if (line.startsWith("+") && !line.startsWith("+++")) { additions++; }
+            else if (line.startsWith("-") && !line.startsWith("---")) { deletions--; }
+        }
+        return { additions, deletions };
     }
 
-    private run(cmd: string): string {
-
-        return execSync(cmd, {
-            cwd: this.workSpaceRoot,
-            encoding: "utf-8",
-            stdio: ["pipe", "pipe", "pipe"],
-        }).trim();
-    }
 }
