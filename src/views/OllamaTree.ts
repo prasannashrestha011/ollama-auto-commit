05d9845b84b29a709bdfa1b0d51cb575741f650a
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-03-26 21:55:44 +0545

    feat: added a collapsible section for displaying workspace changes

--- src/views/OllamaTree.ts
+++ src/views/OllamaTree.ts
@@ -1,10 +1,13 @@
 
 import * as vscode from 'vscode';
 import ExtensionState from '../state/state';
+import { GitService } from '../services/gitServices';
 
 class OllamaItem extends vscode.TreeItem {
-    constructor(label: string, command?: vscode.Command) {
-        super(label);
+    constructor(label: string,
+        public collapsibleState: vscode.TreeItemCollapsibleState = vscode.TreeItemCollapsibleState.None
+        , command?: vscode.Command) {
+        super(label, collapsibleState);
         this.command = command;
     }
 }
@@ -12,6 +15,7 @@ class OllamaItem extends vscode.TreeItem {
 export default class OllamaTreeProvider implements vscode.TreeDataProvider<OllamaItem> {
     private _onDidChangeTreeData = new vscode.EventEmitter<void>();
     readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
+
 
     constructor(private state: ExtensionState) {
 
@@ -23,13 +27,32 @@ export default class OllamaTreeProvider implements vscode.TreeDataProvider<Ollam
         return element;
     }
 
-    getChildren(): OllamaItem[] {
+    async getChildren(element?: OllamaItem): Promise<OllamaItem[]> {
         const model = this.state.getSelectedModel();
         const header = new vscode.TreeItem(`Selected Model: ${model}`);
-        header.contextValue = 'header';
-        header.iconPath = new vscode.ThemeIcon('robot');
-        return [
-            header
-        ];
+        if (!element) {
+            // Root level → header (leaf) + collapsible section
+            const header = new OllamaItem(
+                `Selected Model: ${model}`,
+                vscode.TreeItemCollapsibleState.None // leaf node
+            );
+
+            const collapsible = new OllamaItem(
+                'Changes',
+                vscode.TreeItemCollapsibleState.Collapsed // user can expand/collapse this
+            );
+            collapsible.contextValue = "changesSection";
+
+
+            return [header, collapsible];
+        }
+        if (element.label === "Changes") {
+            const git = await GitService.getInstance();
+            if (!git) { return []; }
+            const files = git?.getUnstagedFiles();
+            console.log(files);
+
+        }
+        return [];
     }
 }
