540c3d66dc5d0ae9deb59bc0e0826f3c31da3210
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-03-27 23:25:09 +0545

    feat: enhance OllamaTreeProvider to include staged and changes sections with proper context values

--- src/views/OllamaTree.ts
+++ src/views/OllamaTree.ts
@@ -2,6 +2,7 @@
 import * as vscode from 'vscode';
 import ExtensionState from '../state/state';
 import { GitService } from '../services/gitServices';
+import path from 'path';
 
 class OllamaItem extends vscode.TreeItem {
     constructor(label: string,
@@ -17,8 +18,13 @@ export default class OllamaTreeProvider implements vscode.TreeDataProvider<Ollam
     readonly onDidChangeTreeData = this._onDidChangeTreeData.event;
 
 
-    constructor(private state: ExtensionState) {
+    constructor(private state: ExtensionState, context: vscode.ExtensionContext) {
 
+        const watcher = vscode.workspace.createFileSystemWatcher("**/.git/index");
+        watcher.onDidChange(() => this.refresh());
+        watcher.onDidCreate(() => this.refresh());
+        watcher.onDidDelete(() => this.refresh());
+        context.subscriptions.push(watcher);
     }
     refresh() {
         this._onDidChangeTreeData.fire();
@@ -37,22 +43,51 @@ export default class OllamaTreeProvider implements vscode.TreeDataProvider<Ollam
                 vscode.TreeItemCollapsibleState.None // leaf node
             );
 
-            const collapsible = new OllamaItem(
+            const changesSection = new OllamaItem(
                 'Changes',
                 vscode.TreeItemCollapsibleState.Collapsed // user can expand/collapse this
             );
-            collapsible.contextValue = "changesSection";
+            changesSection.contextValue = "changesSection";
+
+            const stagedSection = new OllamaItem(
+                'Staged',
+                vscode.TreeItemCollapsibleState.Collapsed,
+            );
+            stagedSection.contextValue = "stagedSection";
 
 
-            return [header, collapsible];
+            return [header, stagedSection, changesSection];
+        }
+
+        const git = await GitService.getInstance();
+        if (element.label === "Staged") {
+            if (!git) { return []; }
+            const stagedFiles = await git.getStagedFiles();
+            return stagedFiles.map(file => this.resolveFilePathAndIcon(git, file));
+
         }
         if (element.label === "Changes") {
-            const git = await GitService.getInstance();
             if (!git) { return []; }
-            const files = git?.getUnstagedFiles();
+            const files = await git?.getUnstagedFiles();
             console.log(files);
+            return files.map(file => this.resolveFilePathAndIcon(git, file));
 
         }
+
+
         return [];
+    }
+
+    private resolveFilePathAndIcon(git: GitService, file: string): OllamaItem {
+
+        const segments = file.split("/");
+        const fileName = segments[segments.length - 1]; // last segment
+        const item = new OllamaItem(fileName, vscode.TreeItemCollapsibleState.None, { title: "Stage file", command: "ollama-auto-commit.stage", arguments: [file] });
+        console.log("repoRoot:", git.getRepoRoot());
+        item.description = file;
+        item.resourceUri = vscode.Uri.file(
+            path.join(git.getRepoRoot(), file)  // needs the absolute path
+        );
+        return item;
     }
 }
