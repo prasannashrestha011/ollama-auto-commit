8560f41a661029934595ecf20a306b8df20aee89
prasannashrestha011 <shresthaprashanna27@gmail.com>
2026-03-27 23:24:14 +0545

    subscribe git commands

--- src/extension.ts
+++ src/extension.ts
@@ -3,13 +3,15 @@ import { GitService } from './services/gitServices';
 import OllamaTreeProvider from './views/OllamaTree';
 import OllamaCmds from './commands/ollamaCmds';
 import ExtensionState from './state/state';
+import { GitCmds } from './commands/gitCmds';
 
 
 export async function activate(context: vscode.ExtensionContext) {
 	const state = new ExtensionState(context);
-	const treeProvider = new OllamaTreeProvider(state);
+	const treeProvider = new OllamaTreeProvider(state, context);
 	const ollamaCmds = new OllamaCmds(state, treeProvider);
 
 	vscode.window.registerTreeDataProvider('ollamaCommitView', treeProvider);
 	context.subscriptions.push(...ollamaCmds.registerCommands());
+	context.subscriptions.push(...await GitCmds.registerCommands());
 }
